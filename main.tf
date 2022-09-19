df5830bff3cc0fdf0f51427e06b1e15b3dd19c75
soroushkhosravi <soroushgol2022@gmail.com>
2022-09-19 09:53:05 +0100

    The version is changed.

--- main.tf
+++ main.tf
@@ -41,11 +41,11 @@ provider "helm" {
     host                   = data.aws_eks_cluster.example.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
     exec {
-      api_version      = "client.authentication.k8s.io/v1beta1"
-      args             = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.example.name]
-      command          = "aws"
-      load_config_file = false
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.example.name]
+      command     = "aws"
     }
+    load_config_file = false
   }
 }
 
