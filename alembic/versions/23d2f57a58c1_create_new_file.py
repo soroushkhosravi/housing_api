9213ea18b7bc0ae5eee9622a5cd14ef898f0913b
soroushkhosravi <soroushgol2022@gmail.com>
2022-10-18 17:36:58 +0100

    The users table will be added.

---
+++ alembic/versions/23d2f57a58c1_create_new_file.py
@@ -0,0 +1,32 @@
+"""create new file
+
+Revision ID: 23d2f57a58c1
+Revises: 
+Create Date: 2022-10-18 16:33:04.057611
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '23d2f57a58c1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    conn = op.get_bind()
+    conn.execute("""CREATE TABLE user (
+          id TEXT PRIMARY KEY,
+          name TEXT NOT NULL,
+          email TEXT UNIQUE NOT NULL,
+          profile_pic TEXT NOT NULL
+        );"""
+    )
+
+
+def downgrade() -> None:
+    conn = op.get_bind()
+    conn.execute("DROP TABLE users")
