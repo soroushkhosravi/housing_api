39fc63dec98db1aa9b49c7686d7d2fe08cea593b
soroushkhosravi <soroushgol2022@gmail.com>
2022-11-08 13:53:36 +0000

    The exec is added to the hel provider.

--- main.tf
+++ main.tf
@@ -110,11 +110,11 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.example.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
-    #exec {
-    #api_version = "client.authentication.k8s.io/v1beta1"
-    #args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.example.name]
-    # command     = "aws"
-    #}
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.example.name]
+      command     = "aws"
+    }
     token = data.aws_eks_cluster_auth.cluster-auth.token
   }
 }
