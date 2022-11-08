5136931b8cc0c7b2abb4f4238d46c0a36fc91c66
soroushkhosravi <soroushgol2022@gmail.com>
2022-11-08 17:30:26 +0000

    back to normal.

--- main.tf
+++ main.tf
@@ -151,9 +151,3 @@ resource "helm_release" "housing-api-remote-release" {
     value = random_password.password.result
   }
 }
-
-data "kubernetes_service" "example" {
-  metadata {
-    name = "housing-api-service-loadbalancer"
-  }
-}
