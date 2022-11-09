c2f3224075162d0594aa926b664852d57d81b02d
soroushkhosravi <soroushgol2022@gmail.com>
2022-11-09 13:18:54 +0000

    The record will be created.

--- main.tf
+++ main.tf
@@ -165,10 +165,18 @@ data "aws_route53_zone" "selected" {
   private_zone = false
 }
 
-resource "aws_route53_record" "example" {
-  zone_id = "data.aws_route53_zone.selected.zone_id"
+data "aws_elb" "example" {
+  name = split("-", split(".", data.kubernetes_service.example.status.0.load_balancer.0.ingress.0.hostname).0).0
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.selected.zone_id
   name    = "example"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [data.kubernetes_service.example.status.0.load_balancer.0.ingress.0.hostname]
+  type    = "A"
+
+  alias {
+    name                   = data.aws_elb.example.dns_name
+    zone_id                = data.aws_elb.example.zone_id
+    evaluate_target_health = true
+  }
 }
