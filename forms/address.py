a6157eb87f1c599ce6a57358f78d57c13dd5469d
soroushkhosravi <soroushgol2022@gmail.com>
2022-11-27 18:01:25 +0000

    The address endpoint is added.

--- forms/address.py
+++ forms/address.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField)
+                     RadioField, SubmitField)
 from wtforms.validators import InputRequired, Length
 
 
 class AddressForm(FlaskForm):
     post_code = StringField('Post Code', validators=[InputRequired()])
+
+    submit = SubmitField('Submit')
