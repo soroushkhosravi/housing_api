1308e75aead54d3c0ea558ae9f3a4f455f4aba6b
soroushkhosravi <soroushgol2022@gmail.com>
2022-12-06 18:16:00 +0000

    The test_user is broken.

---
+++ tests/repositories/test_user.py
@@ -0,0 +1,26 @@
+from repositories.user import UserRepository
+from models.user import User
+from sqlalchemy import create_engine
+import pytest
+from infrastructure.database import db
+from sqlalchemy.orm import Session
+
+def raise_exception():
+    raise Exception(db.Model.metadata.__dict__)
+
+engine = create_engine('sqlite://')
+db.Model.metadata.create_all(engine)
+with Session(engine) as session:
+    session.add(User(
+        google_id = 'google_id',
+        name='Soroush',
+        email = 'a@a.com',
+        profile_pic='picture.com'
+    ))
+    session.commit()
+
+users = session.query(User).filter_by(google_id='google_id').all()
+
+raise Exception(users[0].name)
+
+
