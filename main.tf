932fc851dbc20797eee011df35498694dd149d65
soroushkhosravi <soroushgol2022@gmail.com>
2023-06-08 11:28:58 +0100

    the name of the record is changed.

--- main.tf
+++ main.tf
@@ -243,7 +243,7 @@ data "aws_lb" "foobar" {
 
 resource "aws_route53_record" "abc" {
   zone_id = data.aws_route53_zone.selected.id
-  name    = "test"
+  name    = "abc"
   type    = "A"
 
   alias {
